cd5b138c9932006c7fa1145ed9f3948593766f03
rayees <mailtomdrayees@gmail.com>
2026-02-04 14:30:50 +0530

    Updated sg infra from scratch

--- output.tf
+++ output.tf
@@ -1,7 +1,3 @@
-# It will be used by development and production environments
-
 output "sg_id" {
   value = aws_security_group.allow_all.id
 }
-
- 
--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-variable "vpc_name" {}
+variable "vpc_id" {}
 variable "service_ports" {}
 variable "environment" {}
-variable "vpc_id" {}
+variable "vpc_name" {}
