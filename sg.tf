8d8cdafadc74e358e20c67bf6b31d67158405d53
rayees <mailtomdrayees@gmail.com>
2026-02-05 14:59:34 +0530

    modified sg

---
+++ sg.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "allow_all" {
+  description = "Allow all inbound traffic"
+  vpc_id      = var.vpc_id
+
+  dynamic "ingress" {
+    for_each = var.service_ports
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "${var.vpc_name}-SG"
+    environment = var.environment
+  }
+}
